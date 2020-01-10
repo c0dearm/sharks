90c7382c1721e5a020178cb6ac55385ec857ddd9
Aitor Ruano <codearm@pm.me>
2020-01-10 17:08:30 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,118 @@
+use std::collections::HashMap;
+
+mod mersenne;
+mod math;
+
+/// Generate new [Shamir's secret shares](https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing) or recover secrets from them.
+pub struct SecretShares {
+    min_shares: usize,
+    prime: u128,
+}
+
+impl SecretShares {
+
+    /// Returns a result containing a`SecretShares` instance if parameters are reasonable.
+    /// 
+    /// `security_level` is the index of the [Mersenne prime](https://en.wikipedia.org/wiki/Mersenne_prime) to use as the finite field prime modulo (the higher the more secure, but slower).
+    /// Currently, only up to 12 is supported (`p=127, Mp = 2^127 - 1`).
+    /// 
+    /// If `min_shares` is larger or equal to the Mersenne prime an error is returned, as this configuration would generate insecure shares.
+    /// 
+    /// Example, create an instance with minimum 3 shares to recover a secret and 128 bits of security:
+    /// ```
+    /// let shamir = sharks::SecretShares::new(3, 12);
+    /// assert!(shamir.is_ok());
+    /// ```
+    pub fn new(min_shares: usize, security_level: usize) -> Result<Self, &'static str> {
+        let security_level = std::cmp::min(security_level - 1, mersenne::EXPONENTS.len() - 1);
+
+        let prime = u128::pow(2, mersenne::EXPONENTS[security_level]) - 1;
+
+        if (min_shares as u128) < prime {
+            Ok(SecretShares { min_shares, prime })
+        } else {
+            Err("Minimum shares for recovery is too large for current security level")
+        }
+    }
+
+    /// Given a `secret` returns a result with an iterator which generates shares `(x, f(x))` for x from [1, p).
+    /// 
+    /// If `secret` is larger or equal than the Mersenne prime an error is returned, as it would be irrecoverable.
+    ///  
+    /// Example, generate 10 shares for secret `12345`:
+    /// ```
+    /// # use std::collections::HashMap;
+    /// let shamir = sharks::SecretShares::new(3, 12).unwrap();
+    /// let shares: HashMap<u128, u128> = shamir.iter_shares(12345).unwrap().take(10).collect();
+    /// ```
+    pub fn iter_shares(&self, secret: u128) -> Result<impl Iterator<Item = (u128, u128)>, &str> {
+        if secret < self.prime {
+            let (p, coeffs) = math::compute_coeffs(secret, self.min_shares, self.prime);
+            Ok(math::get_evaluator(coeffs, p))
+        } else {
+            Err("Secret is too large for current security level")
+        }
+    }
+
+    /// Given a set of distinct `shares`, returns a result with the recovered secret.
+    /// 
+    /// If the number of `shares` is less than the number of minimum shares an error is returned as the secret is irrecoverable.
+    /// 
+    /// Example, recover the `12345` secret:
+    /// ```
+    /// let shamir = sharks::SecretShares::new(3, 12).unwrap();
+    /// let shares = shamir.iter_shares(12345).unwrap().take(3).collect();
+    /// let secret = shamir.secret_from(&shares).unwrap();
+    /// assert_eq!(secret, 12345);
+    /// ```
+    pub fn secret_from(&self, shares: &HashMap<u128, u128>) -> Result<u128, &str> {
+        if shares.len() < self.min_shares {
+            Err("Not enough shares to recover secret")
+        } else {
+            Ok(math::lagrange_root(shares, self.prime))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SecretShares;
+
+    #[test]
+    fn test_security_level_range() {
+        let shamir = SecretShares::new(10, 1000).unwrap();
+        assert_eq!(shamir.prime, u128::pow(2, 127) - 1);
+
+        let shamir = SecretShares::new(2, 1).unwrap();
+        assert_eq!(shamir.prime, 3);
+    }
+
+    #[test]
+    fn test_min_shares_too_large() {
+        let shamir = SecretShares::new(3, 1);
+        assert!(shamir.is_err());
+    }
+
+    #[test]
+    fn test_secret_too_large() {
+        let shamir = SecretShares::new(2, 1).unwrap();
+        let shares = shamir.iter_shares(3);
+        assert!(shares.is_err());
+    }
+
+    #[test]
+    fn test_insufficient_shares() {
+        let shamir = SecretShares::new(2, 1).unwrap();
+        let shares = shamir.iter_shares(2).unwrap().take(1).collect();
+        let secret = shamir.secret_from(&shares);
+        assert!(secret.is_err());
+    }
+
+    #[test]
+    fn test_integration() {
+        let shamir = SecretShares::new(10, 128).unwrap();
+        let shares = shamir.iter_shares(12345).unwrap().take(100).collect();
+        let secret = shamir.secret_from(&shares).unwrap();
+        assert_eq!(secret, 12345);
+    }
+}
---
+++ src/math.rs
@@ -0,0 +1,103 @@
+use std::collections::HashMap;
+
+use num_bigint::{BigInt, BigUint};
+use num_traits::Zero;
+use num_traits::cast::ToPrimitive;
+use rand::distributions::{Distribution, Uniform};
+
+/// A module that contains necessary algorithms to compute Shamir's shares and recover secrets
+
+/// Computes `num/(num - b) mod p`, a necessary step to compute the [root of the Lagrange polynomial](https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing#Computationally_efficient_approach).
+/// To find the modulo multiplicative inverse of `num - b` the [Extended Euclidean Algorithm](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation) is used.
+fn div_diff_mod(num: &u128, b: &u128, p: u128) -> u128 {
+    let (mut m, mut x, mut inv, mut den) = (p, 0i128, 1i128, if num < b { p - (b - num) } else { num - b });
+
+    while den > 1 {
+        inv -= ((den/m) as i128)*x;
+        den %= m;
+        std::mem::swap(&mut den, &mut m);
+        std::mem::swap(&mut x, &mut inv);
+    }
+    
+    let mut res = BigInt::from(inv);
+    if inv < 0 { res += p }
+    
+    (num*res % p).to_u128().unwrap()
+}
+
+/// Finds the [root of the Lagrange polynomial](https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing#Computationally_efficient_approach).
+pub fn lagrange_root(points: &HashMap<u128, u128>, p: u128) -> u128 {
+    (points.iter().enumerate()
+    .map(|(j, (xj, yj))|
+        points.iter().enumerate().filter(|(m, _)| *m != j)
+        .map(|(_, (xm, _))|
+            div_diff_mod(xm, xj, p)
+        )
+        .product::<BigUint>()*yj % p
+    )
+    .sum::<BigUint>() % p)
+    .to_u128().unwrap()
+}
+
+/// Generates `k` polynomial coefficients, being the last one `s` and the others randomly generated between `[1, p)`.
+/// Coefficient degrees go from higher to lower in the returned vector order.
+pub fn compute_coeffs(s: u128, k: usize, p: u128) -> (u128, Vec<u128>) {
+    let mut coeffs = Vec::with_capacity(k);
+    let between = Uniform::new(1, p);
+    let mut rng = rand::thread_rng();
+
+    for _ in 1..k {
+        coeffs.push(between.sample(&mut rng));
+    }
+    coeffs.push(s);
+
+    (p, coeffs)
+}
+
+/// Given a set of polynomial coefficients `coeffs` and a modulus `p`, returns an iterator that computes a `(x, f(x) mod p)` point
+/// on each iteration. The iterator starts for `x = 1` and ends at `x = p-1`.
+pub fn get_evaluator(coeffs: Vec<u128>, p: u128) -> impl Iterator<Item = (u128, u128)> {
+    (1..p).map(move |x| (x, coeffs.iter().fold(BigUint::zero(), |acc, c| (acc*x + c) % p).to_u128().unwrap()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{div_diff_mod, lagrange_root, compute_coeffs, get_evaluator};
+
+    #[test]
+    fn div_diff_mod_works() {
+        let res = div_diff_mod(&2, &1, 7);
+        assert_eq!(res, 2);
+
+        let res = div_diff_mod(&1, &2, 7);
+        assert_eq!(res, 6);
+    }
+
+    #[test]
+    fn lagrange_root_works() {
+        let iter = get_evaluator(vec![3,2,1], 7);
+        let values = iter.take(3).collect();
+        let root = lagrange_root(&values, 7);
+        assert_eq!(root, 1);
+
+        let iter = get_evaluator(vec![3,2,5], 7);
+        let values = iter.take(3).collect();
+        let root = lagrange_root(&values, 7);
+        assert_eq!(root, 5);
+    }
+
+    #[test]
+    fn compute_coeffs_works() {
+        let coeffs = compute_coeffs(1, 4, 7);
+        assert_eq!(coeffs.0, 7);
+        assert_eq!(coeffs.1.len(), 4);
+        assert_eq!(coeffs.1[3], 1);
+    }
+
+    #[test]
+    fn evaluator_works() {
+        let iter = get_evaluator(vec![3,2,5], 7);
+        let values: Vec<_> = iter.take(2).collect();
+        assert_eq!(values, vec![(1,3), (2,0)]);
+    }
+}
