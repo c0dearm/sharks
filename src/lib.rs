433cf0a25a0afa20023e733e1835ccddf67ea6c3
Aitor Ruano <codearm@pm.me>
2020-01-22 14:53:08 +0100

    fix use order

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,8 @@ mod share;
 
 use std::collections::HashSet;
 
-pub use share::Share;
 use field::GF256;
+pub use share::Share;
 
 /// Tuple struct which implements methods to generate shares and recover secrets over a 256 bits Galois Field.
 /// Its only parameter is the minimum shares threshold.
